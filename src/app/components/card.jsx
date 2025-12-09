e50efb2f0394251605a1a3b090e3d2a4c039a4c7
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-12-09 16:49:46 -0300

    servicos-att

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -1,15 +1,15 @@
 export default function Card({projeto}) {
     return (
-        <a href="#" class="group relative block h-64 sm:h-80 lg:h-96 text-black">
+        <a class="group relative block h-64 sm:h-80 lg:h-96 text-black">
             <span class="absolute inset-0 border-2 border-dashed border-black"></span>
 
             <div class="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
-                <div class="p-4 pt-0! transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+                <div class="p-4 pt-0! transition-opacity  sm:p-6 lg:p-8">
 
                 <h2 class="mt-4 text-xl font-medium sm:text-2xl">{projeto}</h2>
                 </div>
 
-                <div class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+                <div class="absolute p-4 opacity-0 transition-opacity sm:p-6 lg:p-8">
                 <h3 class="mt-4 text-xl font-medium sm:text-2xl">{projeto}</h3>
 
                 <p class="mt-4 text-sm sm:text-base">
--- src/app/components/servicos.jsx
+++ src/app/components/servicos.jsx
@@ -1,33 +1,50 @@
+"use client";
+import { useState } from "react";
 import Divisor from "./divisor";
 
 export default function Servicos() {
-    return (
-        <section className="bg-gray-800" id="servicos">
-          <Divisor texto={"Serviços"}/>
-            <div class="border-b border-gray-200 dark:border-gray-700">
-              <div role="tablist" class="-mb-px flex gap-4">
-              <button role="tab" aria-selected="true" class="flex items-center gap-2 border-b-2 border-blue-600 px-4 py-2 text-sm font-medium text-blue-600 transition-colors hover:text-blue-700 dark:hover:text-blue-500">
-                  Profile
-              </button>
+  const [aba, setAba] = useState("servico1");
 
-              <button role="tab" aria-selected="false" class="flex items-center gap-2 border-b-2 border-transparent px-4 py-2 text-sm font-medium text-gray-600 transition-colors hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200">
-                Settings
-              </button>
+  return (
+    <section className="bg-gray-800" id="servicos">
+      <Divisor texto={"Serviços"} />
 
-              <button role="tab" aria-selected="false" class="flex items-center gap-2 border-b-2 border-transparent px-4 py-2 text-sm font-medium text-gray-600 transition-colors hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200">
-                Alerts
-              </button>
-              </div>
-            </div>
+      <div className="border-b border-gray-200 dark:border-gray-700">
+        <div role="tablist" className="-mb-px flex gap-4">
 
-            <div role="tabpanel" class="mt-4">
-              <p class="text-gray-700 dark:text-gray-200">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique, quae hic dicta
-                quo facere facilis praesentium a sunt, est quia pariatur nam, modi aut minus iste odio
-                consectetur molestias iusto cupiditate ullam laborum veniam quos officia. Quos, temporibus
-                perspiciatis!
-              </p>
-            </div>
-        </section>
-    )
+          <button
+            aria-pressed={aba === "servico1"}
+            onClick={() => setAba("servico1")}
+            className="flex items-center gap-2 border-b-2 border-blue-600 px-4 py-2 text-sm font-medium text-blue-600 transition-colors hover:text-blue-700 dark:hover:text-blue-500"
+          >
+            Servico 1
+          </button>
+
+          <button
+            aria-pressed={aba === "servico2"}
+            onClick={() => setAba("servico2")}
+            className="flex items-center gap-2 border-b-2 border-transparent px-4 py-2 text-sm font-medium text-gray-600 transition-colors hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200"
+          >
+            Servico 2
+          </button>
+
+          <button
+            aria-pressed={aba === "servico3"}
+            onClick={() => setAba("servico3")}
+            className="flex items-center gap-2 border-b-2 border-transparent px-4 py-2 text-sm font-medium text-gray-600 transition-colors hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200"
+          >
+            Servico 3
+          </button>
+
+        </div>
+      </div>
+
+      <div role="tabpanel" className="mt-4 text-gray-200">
+        {aba === "servico1" && <p>Texto do Servico 1</p>}
+        {aba === "servico2" && <p>Texto do Servico 2</p>}
+        {aba === "servico3" && <p>Texto do Servico 3</p>}
+      </div>
+    </section>
+  );
 }
+ 
